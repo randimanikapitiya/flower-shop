5042091c75fefd2c59af0a63a8f8cc49ec8ce7db
Randima-Nikapitiya <shaninikapitiya@gmail.com>
2025-06-05 07:56:10 +0530

    nav change

--- includes/nav.php
+++ includes/nav.php
@@ -4,7 +4,7 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: var(--secondary);">
     <div class="container">
         <a class="navbar-brand" href="/flower-shop/index.php">
-            <i class="fas fa-seedling me-2" style="color: var(--primary);"></i> Flower Shop
+            <i class="fas fa-seedling me-2" style="color: var(--primary);"></i> Blooming Beauty
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
